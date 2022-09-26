615105e6490ce9b664345da557be0b1fe6f5b036
Janan Akkad <jananakkad@hotmail.co.uk>
2022-09-26 16:22:40 +0100

    added page and brand class details within cli

--- lib/more_brands/cli.rb
+++ lib/more_brands/cli.rb
@@ -5,10 +5,11 @@ class MoreBrands::CLI
         get_available_pages
         list_pages
         get_user_page
-        # get_brands_for_page
+        
+        get_brands_for_page
         # puts""
-        # list_brands
-        # get_user_brand
+        list_brands
+        get_user_brand
         # # get_cigars_for(brand)
         # puts ""
     end
@@ -29,30 +30,51 @@ class MoreBrands::CLI
     end
 
     def get_user_page
-        chosen_page = gets.strip
-        binding.pry
-        # if valid_input(chosen_page, @pages)
-        # end
+        chosen_page = gets.strip.to_i
+        show_page_for(chosen_page) if valid_input(chosen_page, @pages)
+        end
     end
 
     def valid_input(input, data)
         input.to_i <= data.length && input.to_i > 0        
     end
 
+    def show_page_for(chosen_page)
+        page = @pages[chosen_page - 1]
+        puts "Here are the results for your chosen page #{page}"
+        # to implement
+        # MoreBrands::Brands.all.each.with_index(1) do | brand |
+        #     puts brand.name
+        # end
+        # get_user_brand
+    end
+
     # # should the code below be put in its own class called Brand?
-    # def get_brands_for_page
-    #     # to be scraped instead
-    #     @brands = ["A", "B", "C", "D", "E"]
+    def get_brands_for_page
+        # to be scraped instead
+        @brands = ["A", "B", "C", "D", "E"]
+    end
+
+    def list_brands
+        # list_brands
+        @brands.each.with_index(1) do |brand, index| 
+        puts "#{index} #{brand}"
+        end
+    end
+
+    def get_user_brand
+        chosen_brand = gets.strip.to_i
+        show_brand_for(chosen_brand) if valid_input(chosen_brand, @brands)
+    end
     # end
 
-    # def list_brands
-    #     # list_brands
-    #     @brands.each.with_index(1) do |brand, index| 
-    #     puts "#{index} #{brand}"
-    #     end
-    # end
+    def show_brand_for(chosen_brand)
+        brand = @brands[chosen_brand - 1]
+        puts "Here are the results for your chosen brand which is #{brand}"
+        # to implement
+        # MoreBrands::Cigars.all.each.with_index(1) do | cigar |
+        #     puts cigar.name
+        # end
+        # get_user_cigar
+    end 
 
-    # def get_user_brand
-        # chosen_brand = gets.strip
-    # end
-end
